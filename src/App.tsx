d2839408ef8578959d2aff4f4db6027d8a46390b
Kolko Valentin <valiant9191@gmail.com>
2023-03-31 15:18:39 +0300

    init: base project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  var a = 0;
+  return (
+    <div className="App">
+      Hello for basic Typescript React redux module scss with Airbnb style
+    </div>
+  );
+}
+
+export default App;
